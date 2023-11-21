d917a57193ca041ca91c674d45653ed7259ced4d
Camila Mendez <mcmendezmanotas@gmail.com>
2023-11-21 13:40:22 -0500

    adicionando tarea

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/configureStore';
-import App from './components/App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+import ReactDOM from 'react-dom/client';
+import { store } from './redux/store';
+import { App } from 'components/App/App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
+    </Provider>
+  </React.StrictMode>
 );
